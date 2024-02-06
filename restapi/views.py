b26ade9afe8cc7756325bf9aa49713e51413e99e
Yash Mittal <102669147+yashm02@users.noreply.github.com>
2024-02-06 16:05:41 +0530

    Update views.py

--- restapi/views.py
+++ restapi/views.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
 # Create your views here.
 
+class index(APIView):
+    def get(self, request):
+        message = "Hello, World!"
+        return render(request, 'index.html', {'message': message})
 
 def index(requests):
     return HttpResponse("Hello, world. You're at Rest.")
